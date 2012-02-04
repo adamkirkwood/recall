17c749afeaf86627549599f95da6686db0525eb9
Adam Kirkwood <adam@xyentific.com>
2012-02-04 15:21:50 -0800

    adding initial tests for URL service

---
+++ models/url.rb
@@ -0,0 +1,44 @@
+require 'digest/sha1'
+require 'redis'
+
+TOKEN_LENGTH = 0..4
+TOKEN_KEY = "url"
+
+class Url  
+  def initialize(params = {})
+    @url = params[:url] if params[:url]
+    @token = params[:token] if params[:token]
+  end
+  
+  def generate_token(url)
+    hash = Digest::SHA1.hexdigest url
+    token = hash[TOKEN_LENGTH]
+  end
+  
+  def expanded_url
+    @url
+  end
+  
+  def save
+    if @url && !@token
+      redis = Redis.new
+      
+      @token = generate_token(@url)
+      
+      redis.setnx self.class.token_key(@token), @url
+    end
+  end
+  
+  def token
+    @token
+  end
+  
+  def self.token_key(token)
+    "#{TOKEN_KEY}:#{token}"
+  end
+  
+  def self.find_by_token(token)
+    redis = Redis.new    
+    redis.get token_key(token)
+  end
+end
--- recall.rb
+++ recall.rb
@@ -3,3 +3,10 @@ require 'sinatra'
 get '/' do
   'Hello World!'
 end
+
+post '/:url' do
+  
+  if !params[:url]
+    status 400
+  end
+end
